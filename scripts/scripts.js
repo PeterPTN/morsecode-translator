0a341a2dd604656bdbe243ac0b90bcaee86001b8
PeterPTN <peter.p.t.nguyen@gmail.com>
2023-01-12 21:43:00 +1100

    Add logic and basic styling

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+import { upvateValues } from "./update-dom.js";
+import decodeMorse from "./decode-morse.js";
+import codeIntoMorse from "./code-into-morse.js";
+
+const form = document.querySelector(".morse__form-input");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Get form data without using query selector
+    // The classic way with names
+
+})
